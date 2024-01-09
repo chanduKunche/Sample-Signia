58bf7420244a714742fc92ce7bbc3bd2cf71f480
sreejhari <ssreejhari@gmail.com>
2024-01-09 14:04:17 +0530

    app.test.tsx final

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,48 +1,55 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import { it, expect, vi } from "vitest";
+import { cleanup, fireEvent, render, screen, waitFor} from "@testing-library/react";
+import { it,vi, describe, beforeEach, afterEach, expect } from "vitest";
 import * as router from "react-router";
 import App from "./App";
-//import { useNavigate } from "react-router-dom"
 
-it("display heading in the main page", () => {
-  render(<App/>);
-  const text = screen.getByText(/Connect Hearing Aids/i);
-  expect(text);
-});
+describe('App tests', () => {
+  beforeEach(() => {
+    render(<App/>);
+    afterEach(() => {
+      cleanup();
+  });
+  });
+  it("display heading in the main page", () => {
+    const text = screen.findByText("Connect Hearing Aids");
+    expect(text);
+  });
 
-it("display retry button in main page", () => {
-  render(<App/>);
-  const element = screen.getByText(/Retry/i);
-  expect(element);
-});
+  it("display retry button in main page", () => {
+    const element = screen.getByText(/Retry/i);
+    expect(element);
+  });
+  
+  it("display ok button in main page", () => {
+    
+    const element = screen.getByText(/Ok/i);
+    expect(element);
+  });
+  
+  it("display cancel button in main page", () => {
+    
+    const element = screen.getByText(/Cancel/i);
+    expect(element);
+  });
+  
+  const navigate = vi.fn();
+  vi.spyOn(router, "useNavigate").mockImplementation(() => navigate);
+  
+  it("OK button is working or not", async () => {
+   
+    const okButton = screen.getByText(/Ok/i);
+    fireEvent.click(okButton);
+    await waitFor(() =>
+      expect(navigate).toHaveBeenCalledWith("/SelectedHAs", {
+        state: {
+          left: "",
+          right: "",
+        },
+      })
+    );
+  });
+})
 
-it("display ok button in main page", () => {
-  render(<App/>);
-  const element = screen.getByText(/Ok/i);
-  expect(element);
-});
 
-it("display cancel button in main page", () => {
-  render(<App/>);
-  const element = screen.getByText(/Cancel/i);
-  expect(element);
-});
 
-const navigate = vi.fn();
-vi.spyOn(router, "useNavigate").mockImplementation(() => navigate);
-// return navigate
 
-it("OK button is working or not", async () => {
-  render(<App/>);
-
-  const okButton = screen.getByText(/Ok/i);
-  fireEvent.click(okButton);
-  await waitFor(() =>
-    expect(navigate).toHaveBeenCalledWith("/SelectedHAs", {
-      state: {
-        left: "",
-        right: "",
-      },
-    })
-  );
-});
